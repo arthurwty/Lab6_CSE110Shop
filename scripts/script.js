4a6c813975ca5db6f696310e995a8b721df86ad8
arthurw <59636950+arthurwty@users.noreply.github.com>
2021-02-14 14:10:33 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,53 @@
 // Script.js
-
+var myStorage = window.localStorage;
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  if(myStorage.getItem('card') == null){
+    fetch('https://fakestoreapi.com/products').then(response => response.json()).then(data => myStorage.setItem('card',JSON.stringify(data)));
+  }
+  var productArray = JSON.parse(myStorage.getItem('card'));
+  for(var i = 0; i < productArray.length; i++) {
+    var curr = productArray[i];
+    var pr = document.createElement('product-item');
+    const shadow = pr.shadowRoot;
+    shadow.querySelector('.price').textContent = "$" + curr.price;
+    shadow.querySelector('.title').textContent = curr.title;
+    shadow.querySelector('img').src = curr.image;
+    shadow.querySelector('img').alt = curr.title;
+    shadow.querySelector('button').id = i;
+    shadow.querySelector('button').onclick = function(){addCartClick(this)};
+    document.getElementById('product-list').appendChild(pr);
+  }
 });
+function addCartClick(elem){
+  if(!elem.getAttribute('add')){
+    removeCartClick(elem);
+  }else if(elem.getAttribute('add')){
+    if(myStorage.getItem(elem.id) != null){
+      break;
+    }
+    var cartCount = 0;
+    if(myStorage.getItem('count') == null) {
+      myStorage.setItem('count', cartCount);
+    } else {
+      cartCount = myStorage.getItem('count');
+    }
+    cartCount++;
+    myStorage.setItem('count', cartCount);
+    document.getElementById('cart-count').textContent = cartCount;
+    //elem.textContent = "Remove from Cart";
+    elem.textContent = "Remove from Cart";
+    elem.setAttribute('add', true);
+    myStorage.setItem(elem.id,"exist");
+  }
+}
+function removeCartClick(elem){
+  if(myStorage.getItem(elem.id) == null){
+    break;
+  }
+  var cartCount = myStorage.getItem('count');
+  cartCount--;
+  myStorage.setItem('count', cartCount);
+  document.getElementById('cart-count').textContent = cartCount;
+  elem.textContent = "Add to Cart";
+  elem.setAttribute('add', false);
+}
